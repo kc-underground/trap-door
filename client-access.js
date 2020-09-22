5d7348c5b16a70172768c7dc987f78b184cce68f
KC <70451439+kc-underground@users.noreply.github.com>
2020-09-22 13:51:13 -0700

    Update client-access.js

--- client-access.js
+++ client-access.js
@@ -15,4 +15,4 @@ var _0x1786 = function (_0x2af049, _0x5358bc) {
     var _0x178654 = _0x5358[_0x2af049];
     return _0x178654;
 };
-window[_0x1786('0x1')][_0x1786('0x0')] = 'https://drive.google.com/file/d/1V8Zdc31/view?usp=sharing';
+window[_0x1786('0x1')][_0x1786('0x0')] = 'aHR0cHM6Ly93d3cueW91dHViZS5jb20vd2F0Y2g/dj14YlhvV1JzMkM4TQ==';
